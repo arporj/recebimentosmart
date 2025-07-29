967f9b7f7a74a0418e75b33fa95f9c3a7839b8fe
Andre Ricardo <arporj@gmail.com>
2025-07-29 18:25:45 -0300

    Correção dos nomes das variáveis de ambiente

--- netlify/functions/contact-form.js
+++ netlify/functions/contact-form.js
@@ -12,18 +12,18 @@ exports.handler = async (event) => {
     // Configurar o transporter do Nodemailer
     // Use suas credenciais de e-mail aqui, preferencialmente de variáveis de ambiente
     const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: process.env.EMAIL_SECURE === 'true', // true para 465, false para outras portas
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: process.env.SMTP_SECURE === 'true', // true para 465, false para outras portas
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD,
       },
     });
 
     const mailOptions = {
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_TO, // O e-mail para onde as críticas/sugestões serão enviadas
+      from: process.env.SMTP_FROM_EMAIL,
+      to: process.env.RECIPIENT_EMAIL, // O e-mail para onde as críticas/sugestões serão enviadas
       subject: `Nova Crítica/Sugestão de ${name}`,
       html: `
         <p><strong>Nome:</strong> ${name}</p>
