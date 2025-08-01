e0a41e46732896492c088fac49cf8fc5dc40832f
Andre Ricardo <arporj@gmail.com>
2025-08-01 14:54:59 -0300

    Serverless Netlify

--- netlify/functions/express-api.js
+++ netlify/functions/express-api.js
@@ -2,6 +2,9 @@ const serverless = require('serverless-http');
 const app = require('../../server'); // Caminho relativo para o seu arquivo index.js do Express
 
 exports.handler = async (event, context) => {
+  console.log('--- INÍCIO DA FUNÇÃO NETLIFY ---');
+  console.log('Evento recebido (body):', event.body);
+  console.log('Evento recebido (headers):', event.headers);
   console.log('express-api function invoked');
   return serverless(app)(event, context);
 };
