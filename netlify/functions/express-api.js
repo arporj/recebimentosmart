709dc3cf4b2ff759a23faa8aba2c0f2f89959deb
Andre Ricardo <arporj@gmail.com>
2025-08-01 15:07:00 -0300

    Express-api simplificado

--- netlify/functions/express-api.js
+++ netlify/functions/express-api.js
@@ -1,9 +1,7 @@
-const serverless = require('serverless-http');
-const app = require('../../server'); // Caminho relativo para o seu arquivo index.js do Express
-
 exports.handler = async (event, context) => {
-  console.log('--- INÍCIO DA FUNÇÃO NETLIFY ---');
-  console.log('Evento COMPLETO recebido:', JSON.stringify(event, null, 2));
-  console.log('express-api function invoked');
-  return serverless(app)(event, context);
+  console.log('--- TESTE DE LOG SIMPLES DA FUNÇÃO NETLIFY ---');
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Função Netlify de teste executada com sucesso!' }),
+  };
 };
